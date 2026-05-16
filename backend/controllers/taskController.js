bd49a790c145cafa0898f36077a34c01dacce3b2
dodo <douaebeghiele@gmail.com>
2026-05-16 19:19:30 +0200

    setup task controller dependencies and models

---
+++ backend/controllers/taskController.js
@@ -0,0 +1,4 @@
+const Task         = require('../models/Task');
+const Project      = require('../models/Project');
+const Notification = require('../models/Notification');
+const { logActivity } = require('../middlewares/activityLogger');
