af9d89bdbd58ffd547126db0f7938bf07e629950
youngsisi <saraelrebbate@gmail.com>
2026-05-17 00:15:02 +0100

    feat: add active projects metric

--- backend/controllers/dashboardController.js
+++ backend/controllers/dashboardController.js
@@ -6,3 +6,8 @@ const getDashboard = async (req, res) => {
   try {
     const userId = req.user._id;
     const now    = new Date();
+
+    const activeProjects = await Project.countDocuments({
+      status: 'actif',
+      $or: [{ creator: userId }, { members: userId }],
+    });
