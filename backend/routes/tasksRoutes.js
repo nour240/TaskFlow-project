b4b17fc6fbcd62c0b11105194e33938a4b11ac9c
dodo <douaebeghiele@gmail.com>
2026-05-16 20:05:28 +0200

    Protect task routes with authentication

--- backend/routes/tasksRoutes.js
+++ backend/routes/tasksRoutes.js
@@ -1,3 +1,7 @@
 const router = require('express').Router();
 const { protect } = require('../middlewares/auth');
 const { validateTask, validateTaskStatus } = require('../middlewares/validate');
+const {
+  createTask, getTasks, getTask, updateTask, updateTaskStatus, deleteTask,
+} = require('../controllers/taskController');
+router.use(protect);
