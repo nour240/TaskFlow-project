b140743f26902d2ed4c5c0768380c37264157831
dodo <douaebeghiele@gmail.com>
2026-05-18 02:49:53 +0200

    fix:fixer les bugs

--- backend/models/Activity.js
+++ backend/models/Activity.js
@@ -1,24 +1,26 @@
 const mongoose = require('mongoose');
-const activitySchema = new mongoose.Schema(
+const activitySchema = new mongoose.Schema({
     action: {
-  type: String,
-  required: true,
-  trim: true,
-},
-project: {
-  type: mongoose.Schema.Types.ObjectId,
-  ref: 'Project',
-  required: true,
-},
-user: {
-  type: mongoose.Schema.Types.ObjectId,
-  ref: 'User',
-  required: true,
-},
-meta: {
-  type: mongoose.Schema.Types.Mixed,
-  default: {},
+    type: String,
+    required: true,
+    trim: true,
+    },
+    project: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Project',
+    required: true,
+    },
+    user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+    },
+    meta: {
+    type: mongoose.Schema.Types.Mixed,
+    default: {},
+    },
 },
 { timestamps: true }
+);
 activitySchema.index({ project: 1, createdAt: -1 });
 module.exports = mongoose.model('Activity', activitySchema);
